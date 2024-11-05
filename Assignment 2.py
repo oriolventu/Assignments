48889d8a0485c32faae1e41aa339d58a0536ae64
oriolventu <oriol.ventura1@alumni.esade.edu>
2024-11-05 17:24:07 +0100

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -9,15 +9,15 @@
 
 def fizzbuzz(n):
     for i in range (n, n + 1):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0: 
             print("Fizz")
         elif i % 5 == 0:
             print('Buzz')
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
         else:
             print(i)
-
+            
 fizzbuzz(20) 
 
 # Exercise 2: Basic Data Filtering
